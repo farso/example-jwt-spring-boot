5e2ca4ca5657bde9d3394b666b2cbd3bfef783c4
farso <isfores@gmail.com>
2018-08-30 12:26:02 +0200

    Revert to 72384839068c8198cc18fbe188e331abae1d6417

--- src/main/java/com/auth0/samples/authapi/springbootauthupdated/SpringbootAuthUpdatedApplication.java
+++ src/main/java/com/auth0/samples/authapi/springbootauthupdated/SpringbootAuthUpdatedApplication.java
@@ -10,21 +10,9 @@ public class SpringbootAuthUpdatedApplication {
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-		return new BCryptPasswordEncoder();
-	}
-	
-=======
 		return new BCryptPasswordEncoder;
 	}
 
->>>>>>> 7238483... add ApplicationUserRepository with an on-disk DB
-=======
-		return new BCryptPasswordEncoder;
-	}
-
->>>>>>> 72384839068c8198cc18fbe188e331abae1d6417
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootAuthUpdatedApplication.class, args);
 	}
